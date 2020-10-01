6e1a598bedb543a4eeefee7441acbc20ea325675
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2020-10-01 17:34:57 +0530

    firebase auth instance

--- app/src/main/java/com/example/foodie/MainActivity.java
+++ app/src/main/java/com/example/foodie/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.foodie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        updateUI(currentUser);
+    }
+
+
+    public void  updateUI(FirebaseUser account){
+        if(account != null){
+            Toast.makeText(this,"Signed In",Toast.LENGTH_LONG).show();
+//            startActivity(new Intent(this,AnotherActivity.class));
+        }else {
+            Toast.makeText(this,"Sign in Failed",Toast.LENGTH_LONG).show();
+        }
     }
 }
