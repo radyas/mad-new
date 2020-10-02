f70572f052e8b813de4fca300477c849a319dba7
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2020-10-02 11:23:45 +0530

    activity switch

--- app/src/main/java/com/example/foodie/LoginActivity.java
+++ app/src/main/java/com/example/foodie/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.foodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void register(View view) {
+        Intent register = new Intent(this, sign_up.class);
+        startActivity(register);
     }
 }
--- app/src/main/java/com/example/foodie/sign_up.java
+++ app/src/main/java/com/example/foodie/sign_up.java
@@ -2,7 +2,9 @@ package com.example.foodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class sign_up extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void login(View view) {
+        Intent login = new Intent(this, LoginActivity.class);
+        startActivity(login);
     }
 }
