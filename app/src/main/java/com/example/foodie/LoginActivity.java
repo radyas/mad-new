49417c7fa3f96886c121be9a06c1e3173d4636a7
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2020-10-03 11:26:47 +0530

    login complete

--- app/src/main/java/com/example/foodie/LoginActivity.java
+++ app/src/main/java/com/example/foodie/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.example.foodie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -45,10 +53,9 @@ public class LoginActivity extends AppCompatActivity {
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEmail = findViewById(R.id.sign_up_email);
-                mPassword = findViewById(R.id.sign_up_password);
-                mRegister = findViewById(R.id.sign_up_button1);
-                mLogin = findViewById(R.id.sign_up_button2);
+                mEmail = findViewById(R.id.sign_in_email);
+                mPassword = findViewById(R.id.sign_in_password);
+                mLogin = findViewById(R.id.sign_in_button1);
 
                 auth = FirebaseAuth.getInstance();
 
@@ -56,8 +63,45 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                     finish();
                 }
+
+
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is empty.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is empty.");
+                }
+
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    updateUI(user);
+                                }
+                                else {
+                                    Log.w(TAG, "Sign in Failed", task.getException());
+                                    Toast.makeText(LoginActivity.this,
+                                            "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
             }
         });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
     }
 
 }
