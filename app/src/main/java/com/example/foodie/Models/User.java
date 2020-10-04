9a9c3c5643ac58475be3e36dd56ab6306bc52fda
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2020-10-04 18:37:56 +0530

    user model

---
+++ app/src/main/java/com/example/foodie/Models/User.java
@@ -0,0 +1,56 @@
+package com.example.foodie.Models;
+
+public class User {
+    private String userID, name, phone, email, userType;
+
+    public User() {
+    }
+
+    public User(String userID, String name, String phone, String email, String userType) {
+        this.userID = userID;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
